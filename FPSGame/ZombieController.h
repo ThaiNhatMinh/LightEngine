c21653734b41ee1b3aca71284350f4e0a6af4c92
ThaiNhatMinh <t.n.minh1997@gmail.com>
2018-04-18 17:27:22 +0700

    - add more empl

--- FPSGame/ZombieController.h
+++ FPSGame/ZombieController.h
@@ -4,16 +4,25 @@
 class ZombieController : public ActorComponent
 {
 public:
-	ZombieController()=default;
+	ZombieController();
 	~ZombieController() = default;
 	static const char*	Name;
 
 	virtual bool VInit(const tinyxml2::XMLElement* pData);
-	virtual tinyxml2::XMLElement* VGenerateXml(tinyxml2::XMLDocument*p);
-	virtual void VPostInit(void);
+	virtual tinyxml2::XMLElement* VGenerateXml(tinyxml2::XMLDocument*p) { return nullptr; };
+	virtual void VPostInit(void) {};
 	virtual void VUpdate(float dt);
-	virtual void VPostUpdate();
-	virtual void VOnChanged(void);
+	virtual void VPostUpdate() {};
+	virtual void VOnChanged(void) {};
 
 	virtual const char *VGetName() const;
+
+
+	// AI Function
+	bool CanMove();
+protected:
+	void FindTarget(float dt);
+private:
+	Creature * m_pTarget; // Zombie target to attack
+	Game* m_pGame; // Our game
 };
