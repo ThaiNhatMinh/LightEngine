15d0c16906b87fff4c7ac6dbf58774e87f2b280f
Thái Nhật Minh <t.n.minh1997@gmail.com>
2018-12-29 00:33:34 +0700

    * fix pLights uniform not active

--- 3D Project/GameAssets/System/Shader/Default.fs
+++ 3D Project/GameAssets/System/Shader/Default.fs
@@ -1,4 +1,4 @@
-#version 330
+#version 330 core
 #define MAX_POINTLIGHT 10
 struct DirLight
 {
@@ -42,6 +42,7 @@ uniform samplerCube uCubeTex;
 uniform vec3 uCameraPos;
 uniform DirLight dLight;
 uniform PointLight pLights[MAX_POINTLIGHT];
+
 uniform int uNumLight;
 out vec4 Color;
 
@@ -65,31 +66,38 @@ vec4 ComputerDirLight(SurfaceInfo info)
     vec4 diffuse = vec4(dLight.Id,1.0f)* vec4(mat.Kd,1.0f) *diff * info.texel;
     vec4 specular = vec4(dLight.Is,1.0f)* vec4(mat.Ks,1.0f) *spec * info.texel;
 
-    vec4 output = vec4(ambient + diffuse + specular);
+    vec4 result = vec4(ambient + diffuse + specular);
 
-    return output;
+    return result;
 }
 
-vec4 ComputerPointLight(SurfaceInfo info,PointLight pLight)
+vec4 ComputerPointLight(SurfaceInfo info,int i)
 {
-     vec3 LightDir = normalize(pLight.Pos - info.position);
+     vec3 LightDir = normalize(pLights[i].Pos - info.position);
     vec3 reflectDir = reflect(-LightDir,info.normal);
 
     float diff = max(dot(LightDir, info.normal),0.0f);
     float spec = pow(max(dot(info.viewDir, reflectDir),0.0f), mat.shininess);
 
-    vec4 ambient =  vec4(pLight.Ia,1.0f) * vec4(mat.Ka,1.0f) *  info.texel;
-    vec4 diffuse =  vec4(pLight.Id,1.0f) * vec4(mat.Kd,1.0f) * diff * info.texel;
-    vec4 specular = vec4(pLight.Is,1.0f) * vec4(mat.Ks,1.0f) * spec * info.texel;
+    vec4 ambient =  vec4(pLights[i].Ia,1.0f) * vec4(mat.Ka,1.0f) *  info.texel;
+    vec4 diffuse =  vec4(pLights[i].Id,1.0f) * vec4(mat.Kd,1.0f) * diff * info.texel;
+    vec4 specular = vec4(pLights[i].Is,1.0f) * vec4(mat.Ks,1.0f) * spec * info.texel;
 
     // attenuation
-    float distance    = length(pLight.Pos -  info.position);
-    float attenuation = 1.0 / (pLight.constant + pLight.linear * distance + pLight.quadratic * (distance * distance));
+    float distance    = length(pLights[i].Pos -  info.position);
+    float attenuation = 1.0 / (pLights[i].constant + pLights[i].linear * distance + pLights[i].quadratic * (distance * distance));
 
-    vec4 output = vec4(ambient + diffuse + specular);
-    output *= attenuation;
+    vec4 result = vec4(ambient + diffuse + specular);
+    result *= attenuation;
 
-    return output;
+    return result;
+}
+vec4 temp(SurfaceInfo info)
+{
+    vec4 t = vec4(0,0,0,0);
+    for(int i=0; i<uNumLight; i++ )
+        t +=ComputerPointLight(info,i);
+    return t;
 }
 void main()
 {
@@ -100,7 +108,7 @@ void main()
     infoo.viewDir = normalize(uCameraPos - fs_in.oPosition);
 
     Color = ComputerDirLight(infoo);
-    for(int i=0; i<uNumLight; i++ )
-        Color +=ComputerPointLight(infoo,pLights[i]);
-
+    Color +=temp(infoo);
+    
+    
 }
