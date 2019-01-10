1c09723306eb6ee275277fef75b194b6dcce5357
Thái Nhật Minh <t.n.minh1997@gmail.com>
2019-01-10 23:58:10 +0700

    - Update game assets

--- 3D Project/GameAssets/test/script1.lua
+++ 3D Project/GameAssets/test/script1.lua
@@ -3,12 +3,13 @@ tf = GetComponent("Transform")
 
 pos = tf:GetPos()
 pos = pos + glm.vec3.new(100,0,0)
+tf:SetPos(pos)
 --tf:SetPos(pos)
 
 print(pos.x, pos.y, pos.z)
-print(Event.EventType.EvtKeyEvent)
+print(Event.EventType.EvtInput)
 print(Input.KeyAction.Release)
-
+Log("Hello from Lua")
 
 function KeyPress(evt)
 	print(evt.key, evt.action)
